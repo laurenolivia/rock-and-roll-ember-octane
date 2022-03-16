75d24083cf6055a45f4ee0cd95797f5398203ef5
Lauren <lauren.burwell@hashicorp.com>
2022-03-16 14:28:13 -0500

    should there be a bands/band/ dir

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -1,3 +1,8 @@
 import Route from '@ember/routing/route';
 
-export default class BandsBandRoute extends Route {}
+export default class BandsBandRoute extends Route {
+  model(params) {
+    let bands = this.modelFor('bands');
+    return bands.find((band) => band.id === params.id);
+  }
+}
