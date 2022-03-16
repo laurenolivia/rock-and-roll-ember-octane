a2d916679745c5abf3269ea909e6f8f4290fa766
Lauren <lauren.burwell@hashicorp.com>
2022-03-15 19:56:51 -0500

    nested routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,8 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function () {
-  this.route('bands');
-  this.route('songs');
+  this.route('bands', function () {
+    this.route('band', { path: ':id' }, function () {
+      this.route('songs');
+  });
 });
